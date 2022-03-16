6ee1fbe30d0a701c45c3f4680940b0056221c365
bintanghr <haridaramadhan@gmail.com>
2022-03-16 20:54:26 +0700

    menambahkan fungsi penambahan dan pengurangan

---
+++ calculator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
